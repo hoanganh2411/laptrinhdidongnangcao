5cd4bb61b9fcdbb04637c48850a67e2cc5a13cc9
hoanganh2411 <gunnyanh123@gmail.com>
2024-11-17 18:13:02 +0700

    doanlaptrinhdidong

---
+++ lib/components/display_place.dart
@@ -0,0 +1,191 @@
+import 'package:another_carousel_pro/another_carousel_pro.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../Provider/favorite_provider.dart';
+
+
+class DisplayPlace extends StatefulWidget {
+  const DisplayPlace({super.key});
+
+  @override
+  State<DisplayPlace> createState() => _DisplayPlaceState();
+}
+
+class _DisplayPlaceState extends State<DisplayPlace> {
+  // collection for place items
+  final CollectionReference placeCollection =
+      FirebaseFirestore.instance.collection("myAppCpollection");
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    final provider = FavoriteProvider.of(context);
+    return StreamBuilder(
+      stream: placeCollection.snapshots(),
+      builder: (context, streamSnapshot) {
+        if (streamSnapshot.hasData) {
+          return ListView.builder(
+            padding: EdgeInsets.zero,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: streamSnapshot.data!.docs.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final place = streamSnapshot.data!.docs[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                // child: GestureDetector(
+                //   onTap: () {
+                //     Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //         builder: (_) => PlaceDetailScreen(place: place),
+                //       ),
+                //     );
+                //   },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: SizedBox(
+                              height: 375,
+                              width: double.infinity,
+                              child: AnotherCarousel(
+                                images: place['imageUrls']
+                                    .map((url) => NetworkImage(url))
+                                    .toList(),
+                                dotSize: 6,
+                                indicatorBgPadding: 5,
+                                dotBgColor: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 15,
+                            right: 15,
+                            child: Row(
+                              children: [
+                                place['isActive'] == true
+                                    ? Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(40),
+                                        ),
+                                        child: const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 15,
+                                            vertical: 5,
+                                          ),
+                                          child: Text(
+                                            "GuestFavorite",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    : SizedBox(
+                                        width: size.width * 0.03,
+                                      ),
+                                const Spacer(),
+                                // for favorite button
+                                Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    // white border
+                                    const Icon(
+                                      Icons.favorite_outline_rounded,
+                                      size: 34,
+                                      color: Colors.white,
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        provider.toggleFavorite(place);
+                                      },
+                                      child: Icon(
+                                        Icons.favorite,
+                                        size: 30,
+                                        color: provider.isExist(place)
+                                            ? Colors.red
+                                            : Colors.black54,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          // for vendor profile
+                          // vendorProfile(place),
+                        ],
+                      ),
+                      SizedBox(
+                        height: size.height * 0.01,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            place['address'],
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const Spacer(),
+                          const Icon(Icons.star),
+                          const SizedBox(width: 5),
+                          Text(
+                            place['rating'].toString(),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        place['date'],
+                        style: const TextStyle(
+                          fontSize: 16.5,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.007),
+                      RichText(
+                        text: TextSpan(
+                          text: "${place['price']} \đ",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                          children: const [
+                            TextSpan(
+                              text: " đêm",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: size.height * 0.025),
+                    ],
+                  ),
+                // ),
+              );
+            },
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+}
